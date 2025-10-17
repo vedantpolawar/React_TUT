2937afe35eba19c858b9d44147803df6551d7526
vedantpolawar <vedantsunilpolawar@gmail.com>
2025-10-17 15:45:52 +0530

    created currency project

---
+++ 07CurrencyConverter/src/components/index.js
@@ -0,0 +1,5 @@
+// so when ever we create different components for different things we endd to import it in app.js which is very is tedious to import all component one by one
+// so we will create index.js and import all the components here
+import InputBox from "./InputBox";
+
+export { InputBox };
